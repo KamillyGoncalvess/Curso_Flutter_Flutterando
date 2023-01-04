421efbc594bc9fc51fdd01a4250ee8e1f46583c5
Kamilly <gkamilly1607@outlook.com>
2023-01-04 11:13:36 -0300

    Componentização

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/myapp.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-// ignore: use_key_in_widget_constructors
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // ignore: todo
-    // TODO: implement build
-    // ignore: prefer_const_constructors
-    return MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    //throw UnimplementedError();
-    return Material(
-      child: Center(
-        child: Text('Flutterando'),
-      ),
-    );
-  }
 }
